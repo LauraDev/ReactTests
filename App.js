6f66c7f88c9c8ee2c4fa852129ee99143b9da7e1
ltraore <ltraore@Mac-mini-de-ltraore.local>
2018-08-08 15:18:54 +0200

    chap6

---
+++ App.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import Chap2 from './Chap2';
+import Chap3 from './Chap3';
+import Chap4 from './Chap4';
+import Chap5 from './Chap5';
+import Chap6 from './Chap6';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        {/* <Chap2 uri='https://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/Common-dog-behaviors-explained.jpg?itok=FSzwbBoi'/> */}
+        {/* <Chap3/> */}
+        {/* <Chap4/> */}
+        {/* <Chap5/> */}
+        <Chap6/>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: 'white'
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ Chap4.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import Poster from './Poster';
+
+//  Après création d'un élément Poster qui affiche un image de fond, implémenter ce composant dans le slider
+const IMGS = [
+    {link: require('./assets/big/NY1.jpg'), text: 'Image 1'},
+    {link: require('./assets/big/NY2.jpg'), text: 'Image 2'}
+]
+
+export default class Chap4 extends Component {
+    state = {
+        imgPtr: 0
+    }
+
+    componentDidMount() {
+        setInterval( () => {
+            let imgPtr = (this.state.imgPtr + 1) % IMGS.length;
+            this.setState({imgPtr});
+        }, 2000)
+    }
+
+    render() {
+        const img = IMGS[this.state.imgPtr]
+        return (
+            <Poster 
+                background = {img.link} 
+                caption= {img.text} 
+                style={{borderColor: 'white'}}
+            />    
+        )
+        
+    }
+
+}
